dff06142032a8cf2d4477aa8b3ae47c41b24c50d
Nabil Douss <n.douss97@gmail.com>
2023-06-18 11:09:33 +0200

    footer styling choices

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -3,47 +3,54 @@ import * as React from "react"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { ModeToggle } from "@/components/mode-toggle"
-import { Icons } from "./icons"
+import { Icon, Icons } from "./icons"
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
       <div className="flex container justify-between gap-4 md:h-16 md:flex-row md:py-0">
-        <div className="">
+        <div className="mt-1">
           <p className="copyright">&copy; 2023 Nabil Douss </p>
         </div>
-        <div className="flex gap-4"><a
-                href={siteConfig.links.linkedin}
-                target="_blank"
-                rel="noreferrer"
-                className="font-medium flex"
-                >
-                  <Icons.linkedin className="mt-1 mr-2 h-4 w-4" /><span>Linkedin</span>
-                </a><a
-                href={siteConfig.links.stackoverflow}
-                target="_blank"
-                rel="noreferrer"
-                className="font-medium flex"
-                >
-                  <Icons.stackOverflow className="mt-1 mr-2 h-4 w-4" /><span>StackOverflow</span>
-                </a><a
-                href={siteConfig.links.github}
-                target="_blank"
-                rel="noreferrer"
-                className="font-medium flex"
-                >
-                  <Icons.gitHub className="mt-1 mr-2 h-4 w-4" /><span>Github</span>
-                </a><a
-                href={siteConfig.links.email}
-                target="_blank"
-                rel="noreferrer"
-                className="font-medium flex"
-                >
-                  <Icons.mail className="mt-1 mr-2 h-4 w-4" /><span>E-Mail</span>
-                </a>
-            </div>
-            <ModeToggle />
+        <div className="flex gap-4 mt-1">
+        {
+          links.map((link) => (
+            <a
+              href={link.href}
+              key={link.text}
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium flex"
+              >
+                <link.icon className="mt-1 mr-2 h-4 w-4" /><span className="hidden md:flex">{link.text}</span>
+              </a>
+        ))}
+        </div>
+        <ModeToggle />
       </div>
     </footer>
   )
 }
+
+const links: { text: string; href: string; icon: Icon}[] = [
+  {
+    text: "Linkedin",
+    href: siteConfig.links.linkedin,
+    icon: Icons.linkedin,
+  },
+  {
+    text: "StackOverflow",
+    href: siteConfig.links.stackoverflow,
+    icon: Icons.stackOverflow,
+  },
+  {
+    text: "Github",
+    href: siteConfig.links.github,
+    icon: Icons.gitHub,
+  },
+  {
+    text: "E-Mail",
+    href: siteConfig.links.email,
+    icon: Icons.mail,
+  },
+]
